98ad87cb821dc9d540d04a7c5d7d73be988d0a0d
xavikh <xavikh@gmail.com>
2017-06-01 17:17:05 +0200

    MiEvaluacionAdded

---
+++ PracticaEM/PracticaEM/Controllers/MiEvaluacionsController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using PracticaEM.Models;
+
+namespace PracticaEM.Controllers
+{
+    public class MiEvaluacionsController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: MiEvaluacions
+        public ActionResult Index()
+        {
+            return View(db.Evaluacions.ToList());
+        }
+
+        // GET: MiEvaluacions/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Evaluacion evaluacion = db.Evaluacions.Find(id);
+            if (evaluacion == null)
+            {
+                return HttpNotFound();
+            }
+            return View(evaluacion);
+        }
+        
+    }
+}
---
+++ PracticaEM/PracticaEM/Migrations/201706011514141_MiEvaluacionAdded.cs
@@ -0,0 +1,16 @@
+namespace PracticaEM.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class MiEvaluacionAdded : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
